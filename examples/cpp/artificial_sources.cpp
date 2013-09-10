f8fdf26af4138f11b0271de5f2af6f95b0cc6f27
Philippe <Phil.Tillet@gmail.com>
2013-09-10 18:43:30 +0800

    Removed Eigen dependency in example

--- examples/cpp/artificial_sources.cpp
+++ examples/cpp/artificial_sources.cpp
@@ -8,39 +8,45 @@
  * ===========================*/
 
 #include <cmath>
-#include "Eigen/Dense"
-#include "parica.h"
 #include "tests/benchmark-utils.hpp"
+#include "parica.h"
+#include "cblas.h"
 #include <cstdlib>
 
 #define BENCHMARK_COUNT 1
 
-typedef float NumericT;
-typedef Eigen::Matrix<NumericT, Eigen::Dynamic, Eigen::Dynamic,Eigen::RowMajor> MatType;
+typedef float ScalarType;
+typedef Eigen::Matrix<ScalarType, Eigen::Dynamic, Eigen::Dynamic,Eigen::RowMajor> MatType;
 static const unsigned int NC=4;
-static const unsigned int NF=1000;
+static const unsigned int NF=1000000;
 static const unsigned int T=20;
 
 int main(){
-    MatType c_src(NC,NF);
+    ScalarType * src = new ScalarType[NC*NF];
+    ScalarType * mixing = new ScalarType[NC*NC];
+    ScalarType * mixed_src = new ScalarType[NC*NF];
+    ScalarType * independent_components = new ScalarType[NC*NF];
+
     for(unsigned int f=0 ; f< NF ; ++f){
         double t = (double)f/(NF-1)*T - T/2;
-        c_src(0,f) = std::sin(3*t) + std::cos(6*t);
-        c_src(1,f) = std::cos(10*t);
-        c_src(2,f) = std::sin(5*t);
-        c_src(3,f) = std::sin(t*t);
+        src[0*NF + f] = std::sin(3*t) + std::cos(6*t);
+        src[1*NF + f] = std::cos(10*t);
+        src[2*NF + f] = std::sin(5*t);
+        src[3*NF + f]  = std::sin(t*t);
     }
-    MatType mixing(NC,NC);
+
     for(std::size_t i = 0 ; i < NC ; ++i)
         for(std::size_t j = 0 ; j < NC ; ++j)
-			mixing(i,j) = static_cast<double>(rand())/RAND_MAX;
-    MatType mixed_src = mixing*c_src;
-    MatType independent_components(NC,NF);
+            mixing[i*NC+j] = static_cast<double>(rand())/RAND_MAX;
+
+    cblas_sgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,NC,NF,NC,1,mixing,NC,src,NF,0,mixed_src,NF);
+
     fmincl::optimization_options options = parica::make_default_options();
     options.verbosity_level = 2;
     Timer t;
     t.start();
     for(unsigned int i = 0 ; i < BENCHMARK_COUNT ; ++i)
-        parica::inplace_linear_ica(mixed_src.data(),independent_components.data(),NC,NF,options);
+        parica::inplace_linear_ica(mixed_src,independent_components,NC,NF,options);
+
     std::cout << "Execution Time : " << t.get()/BENCHMARK_COUNT << "s" << std::endl;
 }
