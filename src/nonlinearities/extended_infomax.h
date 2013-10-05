0934bdee7863f68df5b7d7fabd7977cd5785c973
Philippe Tillet <Phil.Tillet@gmail.com>
2013-10-05 14:54:39 +0800

    Removed useless and super-absurd viennacl dependency

--- src/nonlinearities/extended_infomax.h
+++ src/nonlinearities/extended_infomax.h
@@ -3,24 +3,15 @@
 
 #include <cstddef>
 
-#include "viennacl/vector.hpp"
-#include "viennacl/matrix.hpp"
 namespace curveica{
 
 template<class ScalarType>
 struct extended_infomax_ica{
-    extended_infomax_ica(std::size_t NC, std::size_t NF) : NC_(NC), NF_(NF), phi_(NF_, NC_), z1_(NF_, NC_), b_(NC_), kurt_(NC_), means_logp_(NC_){}
+    extended_infomax_ica(std::size_t NC, std::size_t NF) : NC_(NC), NF_(NF){}
     void operator()( ScalarType * z1, ScalarType * b, int const * signs, ScalarType* phi, ScalarType* means_logp) const;
 private:
     std::size_t NC_;
     std::size_t NF_;
-
-    viennacl::matrix<ScalarType, viennacl::column_major> phi_;
-    viennacl::matrix<ScalarType, viennacl::column_major> z1_;
-
-    viennacl::vector<ScalarType> b_;
-    viennacl::vector<ScalarType> kurt_;
-    viennacl::vector<ScalarType> means_logp_;
 };
 
 }
