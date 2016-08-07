1c73fc35bed4cfd853d2df8f8327d5305a8e1410
Philippe Tillet <ptillet@g.harvard.edu>
2016-08-06 22:04:44 -0700

    General: Now depending on C++11 instead of TR1 (for RNGs)

--- include/neo_ica/tools/shuffle.hpp
+++ include/neo_ica/tools/shuffle.hpp
@@ -11,7 +11,7 @@
 #define NEO_ICA_TOOLS_SHUFFLE_HPP_
 
 #include <cstddef>
-#include <tr1/random>
+#include <random>
 
 namespace neo_ica
 {
@@ -21,13 +21,11 @@ void shuffle(ScalarType* data, size_t NC, size_t NF){
     size_t* perms = new size_t[NF];
     ScalarType * shuffled_va = new ScalarType[NF];
 
-    std::tr1::minstd_rand gen;
-//  /gen.seed(time(NULL));
-
+    std::minstd_rand gen(0);
     for(size_t i = 0 ; i < NF ; ++i)
         perms[i] = i;
     for(size_t i = 0 ; i < NF ; ++i){
-        size_t j = std::tr1::uniform_int<size_t>(i, NF-1)(gen);
+        size_t j = std::uniform_int_distribution<size_t>(i, NF-1)(gen);
         std::swap(perms[i], perms[j]);
     }
     for(size_t c = 0 ; c < NC ; ++c){
